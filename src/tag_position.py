4133c0f793573dd53b43fe10a4c44960608da5bf
0-keun <dudrms7352@naver.com>
2024-03-20 10:34:15 +0900

    cancel_goal

--- src/tag_position.py
+++ src/tag_position.py
@@ -4,6 +4,7 @@ import rospy
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, Twist
 from std_msgs.msg import Float32
 from visualization_msgs.msg import Marker
+from actionlib_msgs.msg import GoalID
 from triangulation import get_pose
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import math
@@ -44,6 +45,9 @@ class Tag_Position():
         self.turn.linear.x = 0.0
         self.turn.linear.y = 0.0
         self.turn.linear.z = 0.0
+
+        self.cancel_goal = GoalID()
+        self.cancel_publisher = rospy.Publisher('/move_base/cancel',GoalID,queue_size=1)
 
         #############
         self.init_callback_time = rospy.get_time()
@@ -109,12 +113,6 @@ class Tag_Position():
    
         print(self.taginmap)
 
-    def turn_to_tag(self, speed):
-        self.turn.angular.x = 0.0
-        self.turn.angular.y = 0.0
-        self.turn.angular.z = speed
-        self.cmd_vel_publisher.publish(self.turn)
-
     def publish_current_goal(self):
         if math.hypot((self.robot_m[0] - self.tag_m[0][0]),(self.robot_m[1] - self.tag_m[1][0])) > self.dis_threshold:
             if rospy.get_time() - self.last_callback_time > self.callback_interval:
@@ -130,16 +128,16 @@ class Tag_Position():
                 self.goal_msg.pose.orientation.y = rot[1]
                 self.goal_msg.pose.orientation.z = rot[2]
                 self.goal_msg.pose.orientation.w = rot[3]
-                #if self.local_yaw > 0.7:
-                #    self.turn_to_tag(0.3)
-                #elif self.local_yaw < -0.7:
-                #    self.turn_to_tag(-0.3)
                 
                 self.goal_publisher.publish(self.goal_msg)
                 
                 rospy.loginfo("MoveBaseSimpleGoal published: %s", self.goal_msg)
                 
                 self.last_callback_time = rospy.get_time()
+        
+        else:
+            self.cancel_goal.stamp = rospy.Time.now()
+            self.cancel_publisher.publish(self.cancel_goal)
 
     def run(self):
         rospy.spin()
